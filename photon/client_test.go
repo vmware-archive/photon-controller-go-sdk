f258a6d3b306fa6c0ea92a7d78d7dbe36c9539f8
Mihnea Olteanu <molteanu@vmware.com>
2016-03-08 16:40:32 -0800

    client: trim trailing "/" from endpoint url
    when we build urls we assume that the trailing "/" is not present.
    this change is trimming the trailing "/" when the client is initialized
    to ensure we don't build invalid urls
    
    Change-Id: Ic9c13fb06150993aa7415f6ba2af6daba3b3ad9d

---
+++ photon/client_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+//
+// This product is licensed to you under the Apache License, Version 2.0 (the "License").
+// You may not use this product except in compliance with the License.
+//
+// This product may include a number of subcomponents with separate copyright notices and
+// license terms. Your use of these subcomponents is subject to the terms and conditions
+// of the subcomponent's license, as noted in the LICENSE file.
+
+package photon
+
+import (
+	"fmt"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Client", func() {
+	Describe("NewClient", func() {
+		It("Trims trailing '/' from endpoint", func() {
+			endpointList := []string{
+				"http://10.146.1.0/",
+				"http://10.146.1.0",
+			}
+
+			for index, endpoint := range endpointList {
+				client := NewClient(endpoint, "", nil)
+				Expect(client.Endpoint).To(
+					Equal(strings.TrimRight(endpoint, "/")),
+					fmt.Sprintf("Test data index: %v", index))
+			}
+		})
+
+		It("Trims trailing '/' from authEndpoint", func() {
+			endpoint := "http://10.146.1.0/"
+			authEndpointList := []string{
+				"http://10.146.1.1/",
+				"http://10.146.1.0",
+			}
+
+			for index, authEndpoint := range authEndpointList {
+				client := NewClient(endpoint, authEndpoint, nil)
+				Expect(client.AuthEndpoint).To(
+					Equal(strings.TrimRight(authEndpoint, "/")),
+					fmt.Sprintf("Test data index: %v", index))
+			}
+		})
+	})
+})
