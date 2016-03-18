d62be2179d9760101e62ec1a28a46ecf309f55b3
Mihnea Olteanu <molteanu@vmware.com>
2016-03-17 20:11:22 -0700

    test: extract testServer into internal package
    this helper will be used in test for another package that will be added
    shortly to deal with lightwave interactions.
    
    Change-Id: I0402d2ed2d0bd59a6d3b7def28ef356d04b2dd2e
    (cherry picked from commit 6b82b56c4cde49af5375de386777b7395c026388)

--- photon/hosts_test.go
+++ photon/hosts_test.go
@@ -12,11 +12,12 @@ package photon
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/vmware/photon-controller-go-sdk/photon/internal/mocks"
 )
 
 var _ = Describe("Host", func() {
 	var (
-		server   *testServer
+		server   *mocks.Server
 		client   *Client
 		hostSpec *HostCreateSpec
 	)
