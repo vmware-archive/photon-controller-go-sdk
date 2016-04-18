552979f347170bc836164cb9f822e61bce6b96c1
Xiao Zhou <xiaoz@vmware.com>
2016-04-18 10:26:42 -0700

    add list image by name
    Change-Id: Ic8a1e11544fc96d976b6b3011f66d07c80732a82

--- photon/helpers_test.go
+++ photon/helpers_test.go
@@ -162,7 +162,7 @@ func createImage(server *mocks.Server, client *Client) string {
 }
 
 func cleanImages(client *Client) {
-	imageList, err := client.Images.GetAll()
+	imageList, err := client.Images.GetAll(&ImageGetOptions{})
 	if err != nil {
 		GinkgoT().Log(err)
 	}
