b5993fdaa705f389c4abebe7e5d57601aeae2b0c
FatimaIjaz <fijaz@vmware.com>
2016-04-29 00:35:57 -0700

    Added ability to parse token details.
    It provides user with two options:
    1) Either to parses the token into an object.
    2) Or parse the token into json format as a string.
    
    Both of these outputs can eventually be consumed by CLI  for friendly readable output to the user.
    
    Change-Id: I51ae5b0ef460ae15c745090fae535eac7208c6b1

--- photon/lightwave/oidcclient.go
+++ photon/lightwave/oidcclient.go
@@ -10,8 +10,10 @@
 package lightwave
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
 	"fmt"
@@ -233,3 +235,59 @@ func (client *OIDCClient) checkResponse(response *http.Response) (err error) {
 
 	return oidcErr
 }
+
+type JWTToken struct {
+	TokenId    string   `json:"jti"`
+	Algorithm  string   `json:"alg"`
+	Subject    string   `json:"sub"`
+	Audience   []string `json:"aud"`
+	Groups     []string `json:"groups"`
+	Issuer     string   `json:"iss"`
+	IssuedAt   int64    `json:"iat"`
+	Expires    int64    `json:"exp"`
+	Scope      string   `json:"scope"`
+	TokenType  string   `json:"token_type"`
+	TokenClass string   `json:"token_class"`
+	Tenant     string   `json:"tenant"`
+	// It's possible to have more fields depending on how Lightwave defines the token.
+	// This covers all the fields we currently have.
+}
+
+// A JSON web token is a set of Base64 encoded strings separated by a period (.)
+// When decoded, it will either be JSON text or a signature
+// Here we decode the strings into a single token structure. We do not parse the signature.
+func (client *OIDCClient) parseTokenDetails(token string) (jwtToken *JWTToken) {
+	var jwtToken = &JWTToken{}
+
+	chunks := strings.Split(token, ".")
+	for _, chunk := range chunks {
+		json_string, err := base64.RawURLEncoding.DecodeString(chunk)
+		if err == nil {
+			// Ignore errors. We expect that the signature is not JSON,
+			// so unmarshalling it will fail. That's fine. We'll extract
+			// all the data we can.
+			_ = json.Unmarshal(json_string, &jwtToken)
+		}
+	}
+
+	return jwtToken
+}
+
+// A JSON web token is a set of Base64 encoded strings separated by a period (.)
+// When decoded, it will either be JSON text or a signature
+// Here we parse the full JSON text. We do not parse the signature.
+func (client *OIDCClient) parseRawTokenDetails(token string) (jwtToken string, err error) {
+	chunks := strings.Split(token, ".")
+	for _, chunk := range chunks {
+		json_string, err := base64.RawURLEncoding.DecodeString(chunk)
+		if err == nil {
+			var prettyJSON bytes.Buffer
+			err = json.Indent(&prettyJSON, json_string, "", "  ")
+			if err == nil {
+				return string(prettyJSON.Bytes()), nil
+			}
+		}
+	}
+	return token, err
+}
+
