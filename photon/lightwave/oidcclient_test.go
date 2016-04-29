1e22b61ef860e7e222259cd6ce9de1545dc55082
FatimaIjaz <fijaz@vmware.com>
2016-04-29 00:35:57 -0700

    Added ability to parse token details.
    It provides user with two options:
    1) Either to parses the token into an object.
    2) Or parse the token into json format as a string.
    
    Both of these outputs can eventually be consumed by CLI  for friendly readable output to the user.
    
    Change-Id: I51ae5b0ef460ae15c745090fae535eac7208c6b1

--- photon/lightwave/oidcclient_test.go
+++ photon/lightwave/oidcclient_test.go
@@ -365,4 +365,46 @@ var _ = Describe("OIDCClient", func() {
 			client.logger.Printf("RefreshToken: %v\n", resp.RefreshToken)
 		})
 	})
+/*
+	Describe("ParseTokenDetails", func() {
+		Context("with fake server", func() {
+			BeforeEach(func() {
+				client, server = testSetupFakeServer()
+			})
+
+			Context("parsing token details", func() {
+				var (
+					expected *JWTToken
+				)
+
+				BeforeEach(func() {
+					expected := &JWTToken{
+						Subject:  "ec-admin@esxcloud",
+						Groups:   []string{"esxcloud\\ESXCloudAdmins", "esxcloud\\Everyone"},
+						Expires:  1461817527,
+						IssuedAt: 1461817527,
+					}
+					server.SetResponseJsonForPath(tokenPath, 200, expected)
+				})
+
+				It("parses tokens", func() {
+					resp := client.parseTokenDetails(
+						"lc3hjbG91ZCIsInJzX2VzeGNsb3VkIl0sInNjb3BlIjoib3BlbmlkIG9mZmxpbmVfYWNjZXNzIHJzX2VzeGNsb3VkIGF0X" +
+							"2dyb3VwcyIsImlzcyI6Imh0dHBzOlwvXC8xMC4xNDYuNjQuMjM4XC9vcGVuaWRjb25uZWN0XC9lc3hjbG91ZCIsImdyb3V" +
+							"wcyI6WyJlc3hjbG91ZFxcRVNYQ2xvdWRBZG1pbnMiLCJlc3hjbG91ZFxcRXZlcnlvbmUiXSwidG9rZW5fY2xhc3MiOiJhY" +
+							"2Nlc3NfdG9rZW4iLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiZXhwIjoxNDYxODAzMTI3LCJpYXQiOjE0NjE3OTU5MjcsInR" +
+							"lbmFudCI6ImVzeGNsb3VkIiwianRpIjoiQ2ZQYnk3QkFsYU9JM1VqX1RFcV9VSk9KbVlYSmlWT1l1Q1lBWFB3MmwyVSJ9." +
+							"QOpb-8L8if1kEHPEQvsGe_Z8v_gdlPDpjWcu8LxMnAxZELQx6YBn7UM2MO83Qgo-0bqu2ysbcSpjz0mP4pf48z_DyKlMCa" +
+							"B6ViStwavIx7lM1TENrt5nURpjqxlzQY0CxjyYIWxoYQIUbn7c5MXe-vt-OTXAg8bGkwphltj7xUak90mQlZGSBrHFCT_Q" +
+							"PGwxRTNsRwWq45tF7LgKr49L4z5PnkLQ3LpC8jI7x1SUFBiYcJgi76pGNlD4qihpmKhGJK0WpspEAvXhtsGwBVavGxeXzL" +
+							"-PBTYz7Zs1EjD4Isar-91pq-HeTVfhd_KBBqktaQq0WO48Vu0KtHHRv_Us90-Qs53gsY0CnrxHV8qyNR27LyaIMWhG24hq" +
+							"TyBsZVgT-gzs9_-QdLqtkXNgr4Oiqoy9Gi8LAmARGFCgTXOS7uPqZ6_ut71WPhwwoUIuXVUG8vvuRD6_UIIGXyPjBM0sfg" +
+							"X5rMeo45bYO51mNjqAysz7FBwMetkZUqKg6pxWmTmO_xnH5D55I1P2zd_VBo5be-hr7jjTqqDAGkGMU0PM8IajpnWe24wu" +
+							"lPzQqRr5-HlQx50B0nwhYFJVCd_3KW6qCw-MmfGB-1aX-GVG2wa_vUKzc4gDDn65-z0rP_gYtrB9q8oNR-hPY4v18DQEdY" +
+							"bsuoJoqriXk1A0zkeoX13kFXY")
+					Expect(resp).To(BeEquivalentTo(expected))
+				})
+			})
+		})
+	})*/
 })
